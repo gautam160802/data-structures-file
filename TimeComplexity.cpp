4a56fb22f74035b05f4d826b1fb708103e364da9
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-10 12:34:34 +0530

    recall time and space complexity

---
+++ TimeComplexity.cpp
@@ -0,0 +1,23 @@
+// rate at which the time taken increases wrt. input size is called time complexity
+// TC, is define under the worst case scanerio
+// TC, to avoid constants
+// ---to avoid lower values
+// difine by bigO() or O()
+
+/*
+for(i= 0; i< n; i++) {
+
+        for(j = 0; j<= i; j++){
+
+        cout << "running code "
+
+        }
+
+}
+
+here time complexity is n(n+1) /2 or n pow 2
+
+
+
+
+*/
