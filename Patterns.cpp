a3128cb666161f5b5e05a74635641594697095fe
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-11 18:58:15 +0530

    solve 5th pattern problem

--- Patterns.cpp
+++ Patterns.cpp
@@ -23,26 +23,109 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// void print2(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             cout << "*";
+//         }
+
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+
+//     for (int i = 0; i < t; i++)
+//     {
+
+//         int n;
+//         cin >> n;
+
+//         print2(n);
+
+//     }
+// cout << endl;
+//     return 0;
+// }
+/*
+1. for the outer loop, cout the no of lines;
+2. for the inner loop, focus on the column, connect them somewhere to the rows;
+3. print then "*" inside the inner for loop
+4. observe symmetry(optional)
+*/
+// #include <iostream>
+// using namespace std;
+
+// void print3()
+// {
+//     for (int i = 1; i < 6; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+
+//     print3();
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// void print4()
+// {
+//     for (int i = 5; i > 0; i--)
+//     {
+
+//         for (int j = 0; j < i; j++)
+//         {
+
+//             cout << "*" ;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+
+//     print4();
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-
-void print2()
+void print5(int n)
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = 1; j <= n - i + 1; j++)
         {
-            cout << "*";
+            cout << j;
         }
-
         cout << endl;
     }
 }
-
 int main()
 {
-
-    print2();
-
+    int n;
+    cin >> n;
+    print5(n);
     return 0;
 }
