987a35a76b031ce87e66681ed4c17a8710a31133
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-14 10:39:53 +0530

    printing the symeetry star

--- Patterns.cpp
+++ Patterns.cpp
@@ -96,7 +96,7 @@
 //         for (int j = 0; j < i; j++)
 //         {
 
-//             cout << "*" ;
+//             cout << "*";
 //         }
 //         cout << endl;
 //     }
@@ -109,23 +109,23 @@
 //     return 0;
 // }
 
-#include <iostream>
-using namespace std;
-void print5(int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n - i + 1; j++)
-        {
-            cout << j;
-        }
-        cout << endl;
-    }
-}
-int main()
-{
-    int n;
-    cin >> n;
-    print5(n);
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// void print5(int n)
+// {
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i + 1; j++)
+//         {
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     print5(n);
+//     return 0;
+// }
