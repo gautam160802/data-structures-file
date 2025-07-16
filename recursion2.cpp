81bb2138b2538f37c20546ccd9a3cd8e9fcaa9f1
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-16 14:09:13 +0530

    adding recursion_2

--- recursion2.cpp
+++ recursion2.cpp
@@ -27,26 +27,78 @@
 
 // print (1 -n) linearly using recursion
 
+// #include <iostream>
+// using namespace std;
+
+// void print(int i, int n)
+// {
+
+//     if (i > n)
+//     {
+//         return;
+//     }
+//     cout << i << endl;
+
+//     print(i + 1, n);
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     print(1, n);
+
+//     return 0;
+// }
+
+// print (n - 1) linearly using recursion
+// #include <iostream>
+// using namespace std;
+
+// void print(int i, int n)
+// {
+
+//     if (i < 1)
+//     {
+//         return;
+//     }
+//     print(i - 1, n);
+//     cout << i << endl;
+// }
+
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+//     print(n, n);
+//     return 0;
+// }
+
+// print (n - 1) without i-1;
+
+// i
+
 #include <iostream>
 using namespace std;
 
 void print(int i, int n)
 {
-
     if (i > n)
     {
         return;
     }
-    cout << i << endl;
 
     print(i + 1, n);
+    cout << i << endl;
 }
 
 int main()
 {
+
     int n;
     cin >> n;
-    print(1, n);
 
+    print(1, n);
     return 0;
 }
