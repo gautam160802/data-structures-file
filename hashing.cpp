af038be52a5b56f856ef8c178b739668fd13dbe6
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-17 15:14:43 +0530

    string hashing

--- hashing.cpp
+++ hashing.cpp
@@ -1,31 +1,60 @@
 // hashing
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     int hash[13] = {0};
+//     for (int i = 0; i < n; i++)
+//     {
+//         hash[arr[i]] += 1;
+//     }
+
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         int number;
+//         cin >> number;
+
+//         cout << hash[number] << endl;
+//     }
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+    string s;
+    cin >> s;
 
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++)
+    // pre compute
+    int hash[256] = {0};
+    for (int i = 0; i < s.size(); i++)
     {
-        hash[arr[i]] += 1;
+        hash[s[i]]++;
     }
-
     int q;
     cin >> q;
+
     while (q--)
     {
-        int number;
-        cin >> number;
+        char c;
+        cin >> c;
 
-        cout << hash[number] << endl;
+        // fetch
+        cout << hash[c] << endl;
     }
+
     return 0;
 }
