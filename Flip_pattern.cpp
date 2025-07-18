aa69d2392c3ee9b47201cbab23c568eab783ce90
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-18 16:44:36 +0530

    adding upto pattern 13

--- Flip_pattern.cpp
+++ Flip_pattern.cpp
@@ -1,21 +1,84 @@
+// #include <iostream>
+// using namespace std;
+// void print(int n)
+// {
+//     int start = 1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (i % 2 == 0)
+//             start = 1;
+//         else
+//             start = 0;
+
+//         for (int j = 0; j <= i; j++)
+//         {
+
+//             cout << start;
+//             start = 1 - start;
+
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     print(n);
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// void print(int n)
+// {
+//     int space = 2 * (n - 1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         // numbers
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << j;
+//         }
+//         // space
+//         for (int j = 1; j <= space; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         // numbers
+//         for (int j = i; j >= 1; j--)
+//         {
+//             cout << j;
+//         }
+//         cout << endl;
+//         space -= 2;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     print(n);
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
+
+int start = 1;
 void print(int n)
 {
-    int start = 1;
-
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        if (i % 2 == 0)
-            start = 1;
-        else
-            start = 0;
-
-        for (int j = 0; j <= i; j++)
+        for (int j = 1; j <= i; j++)
         {
-
-            cout << start;
-            start = 1 - start;
+            cout << start << " ";
+            start++;
         }
         cout << endl;
     }
@@ -23,6 +86,7 @@ void print(int n)
 
 int main()
 {
+
     int n;
     cin >> n;
     print(n);
