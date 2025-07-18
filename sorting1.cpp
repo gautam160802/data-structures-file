33e052ae9b2a2250495ac19160ef3a7543a005c2
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-18 11:55:53 +0530

    bubble sort is done

--- sorting1.cpp
+++ sorting1.cpp
@@ -1,23 +1,59 @@
-// selection sort
-#include <bits/stdc++.h>
+// // selection sort
+// #include <bits/stdc++.h>
 
+// using namespace std;
+
+// void selection_sort(int arr[], int n)
+// {
+//     for (int i = 0; i <= n - 2; i++)
+//     {
+//         int mini = i;
+//         for (int j = i; j <= n - 1; j++)
+//         {
+//             if (arr[j] < arr[mini])
+//             {
+//                 mini = j;
+//             }
+//         }
+//         int temp = arr[mini];
+//         arr[mini] = arr[i];
+//         arr[i] = temp;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//         cin >> arr[i];
+
+//     selection_sort(arr, n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+// bubble sort
+#include <bits/stdc++.h>
 using namespace std;
 
-void selection_sort(int arr[], int n)
+void bubble_sort(int arr[], int n)
 {
-    for (int i = 0; i <= n - 2; i++)
+    for (int i = n - 1; i >= 0; i--)
     {
-        int mini = i;
-        for (int j = i; j <= n - 1; j++)
+        for (int j = 0; j <= i - 1; j++)
         {
-            if (arr[j] < arr[mini])
+            if (arr[j] > arr[j + 1])
             {
-                mini = j;
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
             }
         }
-        int temp = arr[mini];
-        arr[mini] = arr[i];
-        arr[i] = temp;
     }
 }
 
@@ -27,9 +63,9 @@ int main()
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
-        cin >> arr[i];
 
-    selection_sort(arr, n);
+        cin >> arr[i];
+    bubble_sort(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
