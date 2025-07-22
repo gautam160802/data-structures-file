08c05f5e536b9098509b629cf1e5f1fd1f3f51e1
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-22 10:48:50 +0530

    other method of character triangle

--- Flip_pattern.cpp
+++ Flip_pattern.cpp
@@ -94,17 +94,43 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int start = 1;
+// void print(int n)
+// {
+//     for (int i = n; i > 0; i--)
+//     {
+//         for (char ch = 'A'; ch < 'A' + i; ch++)
+//         {
+//             cout << ch << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+
+//     print(n);
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
-int start = 1;
 void print(int n)
 {
-    for (int i = n; i > 0; i--)
+    for (int i = 0; i < n; i++)
     {
-        for (char ch = 'A'; ch < 'A' + i; ch++)
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
         {
-            cout << ch << " ";
+            cout << ch;
         }
         cout << endl;
     }
@@ -112,10 +138,8 @@ void print(int n)
 
 int main()
 {
-
     int n;
     cin >> n;
-
     print(n);
 
     return 0;
